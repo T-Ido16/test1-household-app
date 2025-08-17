c265fb240d82c518a948f2fd576c49894cc723ac
T.Ido <hakuginhi11414@gmail.com>
2025-08-17 12:07:35 +0900

    6.カレンダーの作成

--- src/components/Calender.tsx
+++ src/components/Calender.tsx
@@ -1,9 +1,78 @@
+import FullCalendar from '@fullcalendar/react'
 import React from 'react'
+import dayGridPlugin from"@fullcalendar/daygrid"
+import jaLocale from "@fullcalendar/core/locales/ja"
+import "../calender.css"
+import { DatesSetArg, EventContentArg } from '@fullcalendar/core'
+import { Balance, CalenderContent, Transaction } from '../types'
+import { calculateDailyBalances } from '../utils/financeCalculations'
+import { formatCurrency } from '../utils/formatting'
 
-const Calender = () => {
+interface CalenderProps {
+  monthlyTransactions: Transaction[]
+  setCurrentMonth: React.Dispatch<React.SetStateAction<Date>>
+}
+
+const Calender = ({monthlyTransactions, setCurrentMonth}: CalenderProps) => {
+  const events = [
+    { title: 'Meeting', start: "2025-08-30", income: 300, expense: 200, balance: 100/*new Date()*/ }
+  ]
+
+  const renderEventContent = (eventInfo: EventContentArg) => {
+    console.log(eventInfo);
+    return (
+      <div>
+        <div className='money' id="event-income">
+          {eventInfo.event.extendedProps.income}
+        </div>
+
+        <div className='money' id="event-expense">
+          {eventInfo.event.extendedProps.expense}
+        </div>
+
+        <div className='money' id="event-balance">
+          {eventInfo.event.extendedProps.balance}
+        </div>
+      </div>
+    )
+  }
+
+  const dailyBalances = calculateDailyBalances(monthlyTransactions)
+  console.log(dailyBalances)
+
+  const createCalenderEvents = (dailyBalances: Record<string, Balance>):CalenderContent[] => {
+    return Object.keys(dailyBalances).map((date) =>{
+      const {income, expense,balance}=dailyBalances[date]
+      return{
+        start: date,
+        income: formatCurrency(income),
+        expense: formatCurrency(expense),
+        balance: formatCurrency(balance),
+      }
+    })
+  }
+
+  const calenderEvents = createCalenderEvents(dailyBalances)
+  console.log(calenderEvents);
+
+  const handleDateSet = (datesetInfo: DatesSetArg) => {
+    console.log(datesetInfo);
+    setCurrentMonth(datesetInfo.view.currentStart)
+  }
+
   return (
-    <div>Calender</div>
+    <FullCalendar 
+      locale={jaLocale}
+      plugins={[dayGridPlugin]}
+      initialView='dayGridMonth'
+      events={calenderEvents}
+      eventContent={renderEventContent}
+      datesSet={handleDateSet}
+    />
   )
 }
+
+
+
 
 export default Calender
